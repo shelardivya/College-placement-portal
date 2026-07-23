f5d08884b32add34f74f237728c4b4b475e6b676
Jayashri Patil <jayashrip892@gmail.com>
2026-07-23 19:20:54 +0530

    img data format fixed

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -11,10 +11,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
-    headers: {
-        "Content-Type": "application/json"
-    }
+    baseURL: import.meta.env.VITE_API_BASE_URL
 });
 
 api.interceptors.request.use((config) => {
