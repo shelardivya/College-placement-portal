ed057b9f7b2bb66bd794afafc3001834d90bc0aa
Jayashri Patil <jayashrip892@gmail.com>
2026-07-23 18:04:16 +0530

    fixed communication

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -18,11 +18,17 @@ const api = axios.create({
     }
 });
 
-
-
-
-
-
+api.interceptors.request.use((config) => {
+    // Nginx strips the first /api from the URL, but the Spring Boot controllers 
+    // for admin and student routes are mapped to /api/admin/... and /api/student/...
+    // So we must manually prepend an extra /api to these specific routes!
+    if (config.url && (config.url.startsWith('/admin/') || config.url.startsWith('/student/'))) {
+        config.url = `/api${config.url}`;
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
 
 
 
